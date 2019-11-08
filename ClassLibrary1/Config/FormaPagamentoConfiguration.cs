d0a309fb9539875d6f6cb56e26a9720cb6306551
edson <ebl4@cin.ufpe.br>
2019-11-08 16:53:18 -0300

    updating mapping configuration

---
+++ ClassLibrary1/Config/FormaPagamentoConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using QuickBuy.Dominio.ObjetoDeValor;
+
+namespace QuickBuy.Repositorio.Config
+{
+    public class FormaPagamentoConfiguration : IEntityTypeConfiguration<FormaPagamento>
+    {
+        public void Configure(EntityTypeBuilder<FormaPagamento> builder)
+        {
+            builder.HasKey(f => f.Id);
+
+            //Builder utiliza o padrão Fluent
+            builder
+                .Property(f => f.Nome)
+                .HasMaxLength(50)
+                .IsRequired();
+
+            builder
+                .Property(f => f.Descricao)
+                .HasMaxLength(100)
+                .IsRequired();
+        }
+    }
+}
