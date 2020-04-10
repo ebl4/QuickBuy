326e701a6fe7881aa500d1dc1ec513b99b269e9a
edson <edbdelima@gmail.com>
2020-04-10 19:37:34 -0300

    updating shipping delete item

--- QuickBuy.Web/ClientApp/src/app/loja/carrinho-compra/loja.carrinho.compra.ts
+++ QuickBuy.Web/ClientApp/src/app/loja/carrinho-compra/loja.carrinho.compra.ts
@@ -2,48 +2,48 @@ import { Produto } from "../../modelo/Produto";
 
 
 export class LojaCarrinhoCompra {
-    public produtos: Produto[] = [];
+  public produtos: Produto[] = [];
 
-    public adicionar(produto: Produto) {
-        var produtoLocalStorage = localStorage.getItem("produtoLocalStorage");
-        if (!produtoLocalStorage) {
-            // se não existir nada no localStorage, atualiza a lista e insere o produto no localStorage
-            this.produtos.push(produto);
-        }
-        else {
-            // se ja existir pelo menos um unico item armazenado na sessao(localLocalStorage)
-            this.produtos = JSON.parse(produtoLocalStorage);
-            this.produtos.push(produto);
-        }
-        localStorage.setItem("produtoLocalStorage", JSON.stringify(this.produtos));
+  public adicionar(produto: Produto) {
+    var produtoLocalStorage = localStorage.getItem("produtoLocalStorage");
+    if (!produtoLocalStorage) {
+      // se não existir nada no localStorage, atualiza a lista e insere o produto no localStorage
+      this.produtos.push(produto);
     }
+    else {
+      // se ja existir pelo menos um unico item armazenado na sessao(localLocalStorage)
+      this.produtos = JSON.parse(produtoLocalStorage);
+      this.produtos.push(produto);
+    }
+    localStorage.setItem("produtoLocalStorage", JSON.stringify(this.produtos));
+  }
 
-    public obterProdutos() : Produto[] {
-        var produtoLocalStorage = localStorage.getItem("produtoLocalStorage");
-        if (produtoLocalStorage)
-            return JSON.parse(produtoLocalStorage);
-        return this.produtos;
-    }
+  public obterProdutos(): Produto[] {
+    var produtoLocalStorage = localStorage.getItem("produtoLocalStorage");
+    if (produtoLocalStorage)
+      return JSON.parse(produtoLocalStorage);
+    return this.produtos;
+  }
 
-    public removerProduto(produto: Produto) {
-        var produtoLocalStorage = localStorage.getItem("produtoLocalStorage");
-        if (produtoLocalStorage) {
-            this.produtos = JSON.parse(produtoLocalStorage);
-            this.produtos = this.produtos.filter(p => p.id != produto.id);
-            localStorage.setItem("produtoLocalStorage", JSON.stringify(this.produtos));
-        }
+  public removerProduto(produto: Produto) {
+    var produtoLocalStorage = localStorage.getItem("produtoLocalStorage");
+    if (produtoLocalStorage) {
+      this.produtos = JSON.parse(produtoLocalStorage);
+      this.produtos = this.produtos.filter(p => p.id != produto.id);
+      localStorage.setItem("produtoLocalStorage", JSON.stringify(this.produtos));
     }
+  }
 
-    public atualizar(produtos: Produto[]) {
-        localStorage.setItem("produtoLocalStorage", JSON.stringify(produtos));
-    }
+  public atualizar(produtos: Produto[]) {
+    localStorage.setItem("produtoLocalStorage", JSON.stringify(produtos));
+  }
 
-    public temItensCarrinhoCompra() {
-        var itens = this.obterProdutos();
-        return (itens.length > 0);
-    }
+  public temItensCarrinhoCompra() {
+    var itens = this.obterProdutos();
+    return (itens.length > 0);
+  }
 
-    public limparCarrinhoCompras() {
-        localStorage.setItem("produtoLocalStorage", "");
-    }
+  public limparCarrinhoCompras() {
+    localStorage.setItem("produtoLocalStorage", "");
+  }
 }
--- QuickBuy.Web/ClientApp/src/app/loja/efetivar/loja.efetivar.component.ts
+++ QuickBuy.Web/ClientApp/src/app/loja/efetivar/loja.efetivar.component.ts
@@ -8,89 +8,91 @@ import { PedidoServico } from "../../servicos/pedido/pedido.servico";
 import { UsuarioServico } from "../../servicos/usuario/usuario.servico";
 
 @Component({
-    selector: "loja-efetivar",
-    templateUrl: "./loja.efetivar.component.html",
-    styleUrls: ["./loja.efetivar.component.css"]
+  selector: "loja-efetivar",
+  templateUrl: "./loja.efetivar.component.html",
+  styleUrls: ["./loja.efetivar.component.css"]
 })
 
-export class LojaEfetivarComponent implements OnInit{
-    public carrinhoCompras: LojaCarrinhoCompra;
-    public produtos: Produto[];
-    public total: number;
+export class LojaEfetivarComponent implements OnInit {
+  public carrinhoCompras: LojaCarrinhoCompra;
+  public produtos: Produto[];
+  public total: number;
 
-    ngOnInit(): void {
-        this.carrinhoCompras = new LojaCarrinhoCompra();
-        this.produtos = this.carrinhoCompras.obterProdutos();
+  ngOnInit(): void {
+    this.carrinhoCompras = new LojaCarrinhoCompra();
+    this.produtos = this.carrinhoCompras.obterProdutos();
+    this.atualizarTotal();
+  }
+
+  constructor(private usuarioServico: UsuarioServico, private pedidoServico: PedidoServico, private router: Router) {
+
+  }
+
+  public atualizarPreco(produto: Produto, quantidade: number) {
+    if (!produto.precoOriginal) {
+      produto.precoOriginal = produto.preco;
+    }
+    if (quantidade <= 0) {
+      quantidade = 1;
+      produto.quantidade = quantidade;
     }
 
-    constructor(private usuarioServico: UsuarioServico, private pedidoServico: PedidoServico, private router: Router) {
+    produto.preco = produto.precoOriginal * quantidade;
+    this.carrinhoCompras.atualizar(this.produtos);
+    this.atualizarTotal();
+  }
 
+  public remover(i, produto : Produto) {
+    this.carrinhoCompras.removerProduto(produto);
+    this.carrinhoCompras.obterProdutos();
+    this.produtos.splice(i, 1);
+    this.atualizarTotal();
+  }
+
+  public atualizarTotal() {
+    //acc: acumulador
+    this.total = this.produtos.reduce((acc, produto) => acc + produto.preco, 0);
+  }
+
+  public efetivarCompra() {
+    console.log("teste");
+    this.pedidoServico.efetivarCompra(this.criarPedido())
+      .subscribe(
+        pedidoId => {
+          console.log(pedidoId);
+          sessionStorage.setItem("pedidoId", pedidoId.toString());
+          this.produtos = [];
+          this.carrinhoCompras.limparCarrinhoCompras();
+          this.router.navigate(["/compra-realizada-sucesso"]);
+        },
+        e => {
+          console.log(e.error);
+        },
+      );
+  }
+
+  public criarPedido(): Pedido {
+    let pedido = new Pedido();
+    pedido.usuarioId = 1;
+    pedido.cep = "30130131";
+    pedido.cidade = "Belo Horizonte";
+    pedido.estado = "Minas Gerais";
+    pedido.dataPrevisaoEntrega = new Date();
+    pedido.formaPagamentoId = 1;
+    pedido.numeroEndereco = "1325";
+    pedido.enderecoCompleto = "Rua Teste 1";
+
+    this.produtos = this.carrinhoCompras.obterProdutos();
+    for (let produto of this.produtos) {
+      let itemPedido = new ItemPedido();
+      itemPedido.produtoId = produto.id;
+      if (!itemPedido.quantidade)
+        itemPedido.quantidade = 1;
+      itemPedido.quantidade = produto.quantidade;
+
+      pedido.itemPedido.push(itemPedido);
     }
 
-    public atualizarPreco(produto: Produto, quantidade: number) {
-        if (!produto.precoOriginal) {
-            produto.precoOriginal = produto.preco;
-        }
-        if (quantidade <= 0) {
-            quantidade = 1;
-            produto.quantidade = quantidade;
-        }
-
-        produto.preco = produto.precoOriginal * quantidade;
-        this.carrinhoCompras.atualizar(this.produtos);
-        this.atualizarTotal();
-    }
-
-    public remover(produto: Produto) {
-        this.carrinhoCompras.removerProduto(produto);
-        this.carrinhoCompras.obterProdutos();
-        this.atualizarTotal();
-    }
-
-    public atualizarTotal() {
-        //o reduce simula o estado (acc, produto) presente no ambiente global env - programação funcional 
-        this.total = this.produtos.reduce((acc, produto) => acc + produto.preco, 0);
-    }
-
-    public efetivarCompra() {
-        console.log("teste");
-        this.pedidoServico.efetivarCompra(this.criarPedido())
-            .subscribe(
-                pedidoId => {
-                    console.log(pedidoId);
-                    sessionStorage.setItem("pedidoId", pedidoId.toString());
-                    this.produtos = [];
-                    this.carrinhoCompras.limparCarrinhoCompras();
-                    this.router.navigate(["/compra-realizada-sucesso"]);
-                },
-                e => {
-                    console.log(e.error);
-                },
-            );
-    }
-
-    public criarPedido(): Pedido {
-        let pedido = new Pedido();
-        pedido.usuarioId = 1;
-        pedido.cep = "30130131";
-        pedido.cidade = "Belo Horizonte";
-        pedido.estado = "Minas Gerais";
-        pedido.dataPrevisaoEntrega = new Date();
-        pedido.formaPagamentoId = 1;
-        pedido.numeroEndereco = "1325";
-        pedido.enderecoCompleto = "Rua Teste 1";
-
-        this.produtos = this.carrinhoCompras.obterProdutos();
-        for (let produto of this.produtos) {
-            let itemPedido = new ItemPedido();
-            itemPedido.produtoId = produto.id;
-            if (!itemPedido.quantidade)
-                itemPedido.quantidade = 1;
-            itemPedido.quantidade = produto.quantidade;
-
-            pedido.itemPedido.push(itemPedido);
-        }
-
-        return pedido;
-    }
+    return pedido;
+  }
 }
